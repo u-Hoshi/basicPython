f9cd882402726546921d3e3b4f59b0a5239caf21
u-Hoshi <yuta000707@yahoo.co.jp>
2021-06-15 12:44:39 +0900

    8章完了

--- chapter8.py
+++ chapter8.py
@@ -32,3 +32,49 @@ print(time.time()-old)
 old=time.time()
 s="一丁目" * 1000000
 print(time.time()-old)
+
+# コマンドライン引数を受け取る
+
+import sys
+total = 0
+for price in sys.argv[1:]:
+    total += int(price)
+print(total)
+
+# キーボードからの入力を受け取る
+
+total = 399
+while total<=300:
+    price =input()
+    total+=int(price)
+    print(total)
+
+# ファイルの入出力
+file = open("greeting.txt","w")
+file.write("hello world")
+file.close()
+
+# JSON
+
+import json
+menu =[
+  {"name":"ハンバーガー","price":100,"calorie":200},
+  {"name":"チーズバーガー","price":360,"calorie":400}
+]
+with open("menu.txt","w") as file:
+    json.dump(menu,file, indent=4)
+
+# jsonデータからpython
+with open("menu.txt","r")as file:
+    menu=json.load(file)
+print(menu)
+
+# 正規表現
+r"[0-9]{7}"
+
+import re
+hoge=re.match(r'[0-9]{7}',"12223442")
+print(hoge)
+
+hoge=re.fullmatch(r'[0-9]{7}',"12223442")
+print(hoge)
