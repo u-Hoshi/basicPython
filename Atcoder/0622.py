6b21dd3a8b1d2939cf3cdc821b333506cb53469a
u-Hoshi <yuta000707@yahoo.co.jp>
2021-06-22 23:52:39 +0900

    0622

--- Atcoder/0622.py
+++ Atcoder/0622.py
@@ -1,5 +1,20 @@
-# C 085
+# C085 B問題
+from collections import Counter
 
+s = int(input()) 
+# リスト内包表記
+A = [int(input()) for _ in range(s)]
+num=0
+
+c = collections.Counter(A)
+
+
+# 出力
+print(num)
+print(A)
+
+# pythonのcollectionsというライブラリーを誤解してしまった。
+# これはローカルにインストールしないと使えないもの！
 
 # 解答
 N = int(input())
