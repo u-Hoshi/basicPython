bf91a41f8310de85e634bbd5d1d87b242584a81a
u-Hoshi <yuta000707@yahoo.co.jp>
2021-10-04 23:52:05 +0900

    ルーティングのGETを作成

--- todo/controllers.py
+++ todo/controllers.py
@@ -1,8 +1,31 @@
+from fastapi import security
+from starlette import requests
 import db
 from models import User, Task
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends,HTTPException
+
+from fastapi.security import HTTPBasic,HTTPBasicCredentials
+
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+import hashlib
+
+def register(request:Request):
+  if request.method=="GET":
+    pass
+  
+  if requests.method=="POST":
+    pass
+  
+  if request.method is 'GET':
+        return templates.TemplateResponse('register.html',
+                                          {'request': request,
+                                           'username': '',
+                                           'error': []})
+
+
 
 app = FastAPI(
   title="FastAPIで作るtodoアプリ",
@@ -14,14 +37,27 @@ app = FastAPI(
 templates = Jinja2Templates(directory="templates")
 jinja_env = templates.env
 
+security = HTTPBasic() 
 
 def index(request:Request):
   return templates.TemplateResponse("index.html",{"request":request,})
 
-def admin(request:Request):
-  user = db.session.query(User).filter(User.username=="admin").first()
-  task = db.session.query(Task).filter(Task.user_id==user.id).all()
+def admin(request:Request,credentials:HTTPBasicCredentials=Depends(security)):
+  username=credentials.username
+  password=hashlib.md5(credentials.password.encode()).hexdigest()
+
+  user = db.session.query(User).filter(User.username==username).first()
+  task = db.session.query(Task).filter(Task.user_id==user.id).all() if user is not None else []
   db.session.close()
+
+  if user is None or user.password != password:
+    error = "ユーザ名かパスワードが間違っています"
+    raise HTTPException(
+      status_code=HTTP_401_UNAUTHORIZED,
+      detail=error,
+      headers={"WWW-Authenticate":"Basic"}
+    )
+
   return templates.TemplateResponse("admin.html",
                                     {"request":request,
                                     "user":user,
--- todo/urls.py
+++ todo/urls.py
@@ -3,3 +3,4 @@ from controllers import *
 
 app.add_api_route("/",index)
 app.add_api_route("/admin",admin)
+app.add_api_route('/register', register, methods=['GET', 'POST'])
