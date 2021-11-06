5e5274433e04d886067b011ed7bd003ad8193cc0
u-Hoshi <yuta000707@yahoo.co.jp>
2021-11-06 23:58:42 +0900

    11/6

--- Atcoder/1030.py
+++ Atcoder/1030.py
@@ -30,7 +30,7 @@ for i in p:
 print(ans)
 
 # B問題
-# 最初の回答
+# 最初の回答 WAになる
 N = int(input())-1
 xy = [map(int, input().split()) for _ in range(N)]
 x, y = [list(i) for i in zip(*xy)]
@@ -41,3 +41,37 @@ if ans==N:
   print("Yes")
 else:
   print("No")
+
+# 別解
+import sys
+input = sys.stdin.readline
+N = int(input())
+l = [0]*N
+for _ in range(N-1):
+    a,b = map(int,input().split()) # それぞれの頂点からいくつ線が出ているかを確認
+    l[a-1] += 1
+    l[b-1] += 1
+if max(l) == N-1: # ここでいう星とは一つの頂点から他の全ての頂点に向けて線が伸びている状態
+                  # →頂点の数-1であれば星だと言える
+    print("Yes")
+else:
+    print("No")
+
+# 別解
+N = int(input())
+ab = [map(int, input().split()) for _ in range(N-1)]
+a, b = [list(i) for i in zip(*ab)]
+ 
+res = [0 for _ in range(N)]
+ 
+for i in range(N-1):
+    res[a[i]-1] +=1
+    res[b[i]-1] +=1
+    
+for i in range(N):
+    if res[i] == N-1:
+        print("Yes")
+        break
+    
+    elif i == N-1:
+        print("No")
