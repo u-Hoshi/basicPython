c57fb60683d716df821cbe2c5c525934a00e07ed
u-Hoshi <yuta000707@yahoo.co.jp>
2021-11-16 23:06:24 +0900

    データベースの作成

---
+++ FastAPI1/ReservationSystem/sql_app/models.py
@@ -0,0 +1,24 @@
+from re import T
+from sqlalchemy import Column,ForeignKey,Integer,String,DateTime
+from sqlalchemy.sql.coercions import InElementImpl
+from sqlalchemy.sql.expression import null
+from .database import Base
+
+class User(Base):
+  __tablename__="users"
+  user_id=Column(Integer,primary_key=True,index=True)
+  username=Column(String,unique=True,index=True)
+
+class Room(Base):
+  __tablename__="rooms"
+  room_id=Column(Integer,primary_key=True,index=True)
+  room_name=Column(String, unique=True,index=True)
+  capacity=Column(Integer)
+
+class Booking(Base):
+  booking_id=Column(Integer,primary_key=True,index=True)
+  user_id=Column(Integer,ForeignKey("users.user_id"),ondelete="SET NULL",nullable=False)
+  room_id=Column(Integer,ForeignKey("rooms.room_id"),ondelete="SET NULL",nullable=False)
+  booked_num=Column(Integer)
+  start_datetime=Column(DateTime,nullable=False)
+  end_datetime=Column(DateTime,nullable=False)
