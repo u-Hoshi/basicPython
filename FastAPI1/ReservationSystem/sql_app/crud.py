5cd8abee1950b1f1662ba7fdc390c5174f8289d5
u-Hoshi <yuta000707@yahoo.co.jp>
2021-11-18 22:17:14 +0900

    createの作成

--- FastAPI1/ReservationSystem/sql_app/crud.py
+++ FastAPI1/ReservationSystem/sql_app/crud.py
@@ -12,3 +12,33 @@ def get_rooms(db:Session,skip:int=0,limit=100):
 # 予約一覧取得(Read)
 def get_bookings(db:Session,skip:int=0,limit=100):
   return db.query(models.Booking).offset(skip).limit(limit).all()  
+
+# ユーザー登録(Create)
+def create_user(db:Session,user:schemas.User):
+  db_user=models.User(username=user.username) # インスタンスの作成
+  db.add(db_user)
+  db.commit()
+  db.refresh(db_user)
+  return db_user
+
+# 会議室の登録
+def create_room(db:Session,room:schemas.Room):
+  db_room=models.Room(room_name=room.room_name, capacity=room.capacity)
+  db.add(db_room)
+  db.commit()
+  db.refresh(db_room)
+  return db_room
+
+# 予約の登録
+def create_booking(db:Session,booking:schemas.Booking):
+  db_booking=models.booking(
+    user_id=booking.user_id,
+    room_id=booking.room_id,
+    booked_num=booking.booked_num,
+    start_datetime=booking.start_datetime,
+    end_datetime=booking.end_datetime
+  )
+  db.add(db_booking)
+  db.commit()
+  db.refresh(db_booking)
+  return db_booking
