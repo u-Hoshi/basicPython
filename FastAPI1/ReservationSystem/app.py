0f0b2a460552b5719a6689663b6d5cc32654df8b
u-Hoshi <yuta000707@yahoo.co.jp>
2021-11-20 09:45:58 +0900

    blackの整形

--- FastAPI1/ReservationSystem/app.py
+++ FastAPI1/ReservationSystem/app.py
@@ -4,103 +4,76 @@ import random
 import requests
 import json
 
-page=st.sidebar.selectbox("choose your page",["users","rooms","bookings"])
+page = st.sidebar.selectbox("choose your page", ["users", "rooms", "bookings"])
 
-if page=="users":
-  st.title("APIテスト画面(ユーザー)")
-  with st.form(key='user'):
-    user_id : int = random.randint(0,10)
-    username : str = st.text_input('ユーザー名',max_chars=12)
-    data={
-      'user_id':user_id,
-      'username':username
-    }
-    submit_button=st.form_submit_button(label='リクエスト送信')
-  if submit_button:
-    st.write("送信データ")
-    st.json(data)
-    st.write("レスポンスの結果")
-    url='http://127.0.0.1:8000/users'
-    res = requests.post(
-      url,
-      data=json.dumps(data)
-    )
-    st.write(res.status_code)
-    st.json(res.json())
-elif page=="rooms":
-  st.title("APIテスト画面(会議室)")
-  with st.form(key='room'):
-      room_id : int = random.randint(0,10) # 自動採番
-      room_name : str = st.text_input('会議室名',max_chars=12)
-      capacity:int=st.number_input("店員",step=1)
-      data={
-        'room_id':room_id,
-        'room_name':room_name,
-        'capacity':capacity
-      }
-      submit_button=st.form_submit_button(label='リクエスト送信')
-  if submit_button:
-    st.write("送信データ")
-    st.json(data)
-    st.write("レスポンスの結果")
-    url='http://127.0.0.1:8000/rooms'
-    res = requests.post(
-      url,
-      data=json.dumps(data)
-    )
-    st.write(res.status_code)
-    st.json(res.json())
-elif page=="bookings":
-  st.title("APIテスト画面(予約)")
-  with st.form(key='booking'):
-      booking_id : int = random.randint(0,10)
-      user_id : int = random.randint(0,10)
-      room_id : int = random.randint(0,10)
-      booked_num:int=st.number_input("予約人数",step=1)
-      date=st.date_input("日付を入力",min_value=datetime.date.today())
-      start_time=st.time_input("開始時刻:",value=datetime.time(hour=9,minute=0))
-      end_time=st.time_input("終了時刻:",value=datetime.time(hour=20,minute=0))
+if page == "users":
+    st.title("APIテスト画面(ユーザー)")
+    with st.form(key="user"):
+        user_id: int = random.randint(0, 10)
+        username: str = st.text_input("ユーザー名", max_chars=12)
+        data = {"user_id": user_id, "username": username}
+        submit_button = st.form_submit_button(label="リクエスト送信")
+    if submit_button:
+        st.write("送信データ")
+        st.json(data)
+        st.write("レスポンスの結果")
+        url = "http://127.0.0.1:8000/users"
+        res = requests.post(url, data=json.dumps(data))
+        st.write(res.status_code)
+        st.json(res.json())
+elif page == "rooms":
+    st.title("APIテスト画面(会議室)")
+    with st.form(key="room"):
+        room_id: int = random.randint(0, 10)  # 自動採番
+        room_name: str = st.text_input("会議室名", max_chars=12)
+        capacity: int = st.number_input("店員", step=1)
+        data = {"room_id": room_id, "room_name": room_name, "capacity": capacity}
+        submit_button = st.form_submit_button(label="リクエスト送信")
+    if submit_button:
+        st.write("送信データ")
+        st.json(data)
+        st.write("レスポンスの結果")
+        url = "http://127.0.0.1:8000/rooms"
+        res = requests.post(url, data=json.dumps(data))
+        st.write(res.status_code)
+        st.json(res.json())
+elif page == "bookings":
+    st.title("APIテスト画面(予約)")
+    with st.form(key="booking"):
+        booking_id: int = random.randint(0, 10)
+        user_id: int = random.randint(0, 10)
+        room_id: int = random.randint(0, 10)
+        booked_num: int = st.number_input("予約人数", step=1)
+        date = st.date_input("日付を入力", min_value=datetime.date.today())
+        start_time = st.time_input("開始時刻:", value=datetime.time(hour=9, minute=0))
+        end_time = st.time_input("終了時刻:", value=datetime.time(hour=20, minute=0))
 
-      data={
-        'booking_id':booking_id,
-        'room_id':room_id,
-        'user_id':user_id,
-        'booked_num':booked_num,
-        'start_datetime':datetime.datetime(
-          year=date.year,
-          month=date.month,
-          day=date.day,
-          hour=start_time.hour,
-          minute=start_time.minute
-        ).isoformat(),
-        'end_datetime':datetime.datetime(
-          year=date.year,
-          month=date.month,
-          day=date.day,
-          hour=end_time.hour,
-          minute=end_time.minute
-        ).isoformat()
-      }
-      submit_button=st.form_submit_button(label='リクエスト送信')
-  if submit_button:
-    st.write("送信データ")
-    st.json(data)
-    st.write("レスポンスの結果")
-    url='http://127.0.0.1:8000/bookings'
-    res = requests.post(
-      url,
-      data=json.dumps(data)
-    )
-    st.write(res.status_code)
-    st.json(res.json())
-
-
-
-
-
-
-
-
-
-
-
+        data = {
+            "booking_id": booking_id,
+            "room_id": room_id,
+            "user_id": user_id,
+            "booked_num": booked_num,
+            "start_datetime": datetime.datetime(
+                year=date.year,
+                month=date.month,
+                day=date.day,
+                hour=start_time.hour,
+                minute=start_time.minute,
+            ).isoformat(),
+            "end_datetime": datetime.datetime(
+                year=date.year,
+                month=date.month,
+                day=date.day,
+                hour=end_time.hour,
+                minute=end_time.minute,
+            ).isoformat(),
+        }
+        submit_button = st.form_submit_button(label="リクエスト送信")
+    if submit_button:
+        st.write("送信データ")
+        st.json(data)
+        st.write("レスポンスの結果")
+        url = "http://127.0.0.1:8000/bookings"
+        res = requests.post(url, data=json.dumps(data))
+        st.write(res.status_code)
+        st.json(res.json())
--- FastAPI1/ReservationSystem/sql_app/crud.py
+++ FastAPI1/ReservationSystem/sql_app/crud.py
@@ -1,44 +1,49 @@
 from sqlalchemy.orm import Session
-from . import models,schemas
+from . import models, schemas
 
 # ユーザー一覧取得(Read)
-def get_users(db:Session,skip:int=0,limit=100):
-  return db.query(models.User).offset(skip).limit(limit).all()
+def get_users(db: Session, skip: int = 0, limit=100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
 
 # 会議室一覧取得(Read)
-def get_rooms(db:Session,skip:int=0,limit=100):
-  return db.query(models.Room).offset(skip).limit(limit).all()
+def get_rooms(db: Session, skip: int = 0, limit=100):
+    return db.query(models.Room).offset(skip).limit(limit).all()
+
 
 # 予約一覧取得(Read)
-def get_bookings(db:Session,skip:int=0,limit=100):
-  return db.query(models.Booking).offset(skip).limit(limit).all()  
+def get_bookings(db: Session, skip: int = 0, limit=100):
+    return db.query(models.Booking).offset(skip).limit(limit).all()
+
 
 # ユーザー登録(Create)
-def create_user(db:Session,user:schemas.User):
-  db_user=models.User(username=user.username) # インスタンスの作成
-  db.add(db_user)
-  db.commit()
-  db.refresh(db_user)
-  return db_user
+def create_user(db: Session, user: schemas.User):
+    db_user = models.User(username=user.username)  # インスタンスの作成
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
 
 # 会議室の登録
-def create_room(db:Session,room:schemas.Room):
-  db_room=models.Room(room_name=room.room_name, capacity=room.capacity)
-  db.add(db_room)
-  db.commit()
-  db.refresh(db_room)
-  return db_room
+def create_room(db: Session, room: schemas.Room):
+    db_room = models.Room(room_name=room.room_name, capacity=room.capacity)
+    db.add(db_room)
+    db.commit()
+    db.refresh(db_room)
+    return db_room
+
 
 # 予約の登録
-def create_booking(db:Session,booking:schemas.Booking):
-  db_booking=models.booking(
-    user_id=booking.user_id,
-    room_id=booking.room_id,
-    booked_num=booking.booked_num,
-    start_datetime=booking.start_datetime,
-    end_datetime=booking.end_datetime
-  )
-  db.add(db_booking)
-  db.commit()
-  db.refresh(db_booking)
-  return db_booking
+def create_booking(db: Session, booking: schemas.Booking):
+    db_booking = models.booking(
+        user_id=booking.user_id,
+        room_id=booking.room_id,
+        booked_num=booking.booked_num,
+        start_datetime=booking.start_datetime,
+        end_datetime=booking.end_datetime,
+    )
+    db.add(db_booking)
+    db.commit()
+    db.refresh(db_booking)
+    return db_booking
