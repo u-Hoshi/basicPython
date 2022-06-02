6575cb5aa572b000601627d01b20570f5aaadee8
u-Hoshi <yuta000707@yahoo.co.jp>
2022-06-02 23:37:08 +0900

    viewsを作成

---
+++ djangovue/jobs/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from jobs.models import JobOffer
+from jobs.api.serializers import JobOffersSerializer
+
+
+class ListView(generics.ListCreateAPIView):
+    queryset = JobOffer.objects.all().order_by("-id")
+    serializer_class = JobOffersSerializer
+
+
+class DetaView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = JobOffer.objects.all()
+    serializer_class = JobOffersSerializer
