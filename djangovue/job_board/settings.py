ef755994b8b5511c9092449ea5ad19515e2ffc5d
u-Hoshi <yuta000707@yahoo.co.jp>
2022-06-03 09:01:52 +0900

    ページネーションの設定

--- djangovue/job_board/settings.py
+++ djangovue/job_board/settings.py
@@ -114,3 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+
+REST_FRAMEWORK = {
+    "PAGE_SIZE": 4,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+}
