f196eaab899c3f96d54ec83d1d84a6202647f9a2
u-Hoshi <yuta000707@yahoo.co.jp>
2022-06-03 09:33:59 +0900

    タイポとdb再作成

--- djangovue/jobs/api/serializers.py
+++ djangovue/jobs/api/serializers.py
@@ -1,6 +1,5 @@
 # Serializerとは、クエリセットやモデルインスタンスのような複雑なデータをJSON形式のフォーマットに変換する
 
-from dataclasses import field
 from rest_framework import serializers
 from jobs.models import JobOffer
 
@@ -8,4 +7,4 @@ from jobs.models import JobOffer
 class JobOffersSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobOffer
-        field = "__all__"
+        fields = "__all__"
