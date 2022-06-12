b76c75e2f370e998fe9422878ff924a063111890
u-Hoshi <yuta000707@yahoo.co.jp>
2022-06-12 13:49:20 +0900

    htmlファイルを追加

--- djangobasic/hellowworldproject/hellowworldproject/urls.py
+++ djangobasic/hellowworldproject/hellowworldproject/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import helloworldfunction
+from .views import HellowWorldClass, helloworldfunction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('helloworld/', helloworldfunction),
+    path('helloworld2/', HellowWorldClass.as_view()), # クラスを使用する場合は".as_view()"をつける必要がある
 ]
--- djangobasic/hellowworldproject/hellowworldproject/views.py
+++ djangobasic/hellowworldproject/hellowworldproject/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def helloworldfunction(request):
   returnedobject=HttpResponse("<h1>hellow world</h1>")
   return returnedobject
+
+class HellowWorldClass(TemplateView):
+  template_name: str = "hello.html" 
