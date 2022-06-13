26bb60a6022e4af274f2213b69bb2d784db3cbb5
u-Hoshi <yuta000707@yahoo.co.jp>
2022-06-13 09:39:49 +0900

    タイトルを返す

--- booklist/bookproject/book/models.py
+++ booklist/bookproject/book/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Book(models.Model):
   title=models.CharField(max_length=50)
   author=models.CharField(max_length=50)
+
+  def __str__(self) -> str: # タイトルを返してあげることで~~~ object(1)ではなくタイトルを表示させることができる
+    return self.title
