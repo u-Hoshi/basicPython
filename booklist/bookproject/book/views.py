3af8784cb74f5828c643e7dd26b0847d8ffb6d5b
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-02 10:33:17 +0900

    IsAuthenticatedの実装

--- booklist/bookproject/book/views.py
+++ booklist/bookproject/book/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from .selializers import BookSerializer
 from .models import Book
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -13,4 +14,4 @@ class BookList(ListView):
 class BookListAPI(ListCreateAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
-  permission_classes=[]
+  permission_classes=[IsAuthenticated]
