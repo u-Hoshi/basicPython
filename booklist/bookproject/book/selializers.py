a776cd6615b76db9ba325d833455a7e0a1606fa9
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-02 10:23:02 +0900

    LIstCreateAPIViewとserializerの実装

---
+++ booklist/bookproject/book/selializers.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Book
+    field=["title","author"]
--- booklist/bookproject/book/views.py
+++ booklist/bookproject/book/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from bookproject.book.selializers import BookSerializer
 from .models import Book
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 
 # Create your views here.
 
@@ -9,5 +10,7 @@ class BookList(ListView):
   template_name: str="list.html"
   model = Book
 
-class BookListAPI(APIView):
+class BookListAPI(ListCreateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
   permission_classes=[]
