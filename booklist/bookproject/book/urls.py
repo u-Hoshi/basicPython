41189532264d812f809e4459e7fdf5a9622bf6cd
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-02 10:14:44 +0900

    APIViewの実装

--- booklist/bookproject/book/urls.py
+++ booklist/bookproject/book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import  path
-from .views import BookList
+from .views import BookList, BookListAPI
 
 urlpatterns = [
     path('list/',BookList.as_view()),
+    path('api/',BookListAPI.as_view()),
 ]
--- booklist/bookproject/book/views.py
+++ booklist/bookproject/book/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Book
+from rest_framework.views import APIView
 
 # Create your views here.
 
 class BookList(ListView):
   template_name: str="list.html"
   model = Book
+
+class BookListAPI(APIView):
+  permission_classes=[]
