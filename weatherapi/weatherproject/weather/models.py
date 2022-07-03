5dfd2f66a25c764355ab617fdf5bd210f18bc6ef
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-03 23:13:31 +0900

    temperatureを追加

--- weatherapi/weatherproject/weather/models.py
+++ weatherapi/weatherproject/weather/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Weather(models.Model):
   location = models.CharField(max_length=32)
   weather = models.CharField(max_length=32)
-  weather = models.IntegerField()
+  temperature = models.IntegerField()
 
   def __str__(self):
     return self.location
