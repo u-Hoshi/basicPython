a08fff074eb9a42b898db6cfbf8a140f65823244
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-04 23:43:45 +0900

    APIViewの作成

--- weatherapi/weatherproject/weather/urls.py
+++ weatherapi/weatherproject/weather/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TopView
+from .views import TopView, WeatherAPIView
 
 urlpatterns = [
-  path("top/", TopView.as_view())
+  path("top/", TopView.as_view()),
+  path("api/", WeatherAPIView.as_view())
 ]
--- weatherapi/weatherproject/weather/views.py
+++ weatherapi/weatherproject/weather/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import WeatherSerializer
 from .models import Weather
 
 # Create your views here.
@@ -7,3 +11,8 @@ from .models import Weather
 class TopView(ListView):
   model = Weather
   template_name: str = "top.html"
+
+class WeatherAPIView(RetrieveAPIView):
+  queryset = Weather.objects.all()
+  serializer_class = WeatherSerializer
+  permission_classes =[IsAuthenticated]
