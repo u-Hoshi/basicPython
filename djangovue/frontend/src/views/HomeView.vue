a4bb942674931a011fe15cd8c272eef9e6022a26
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-17 00:02:22 +0900

    GETとそのデータ表示

--- djangovue/frontend/src/views/HomeView.vue
+++ djangovue/frontend/src/views/HomeView.vue
@@ -1,15 +1,33 @@
 <template>
-  <hello-world />
+  <div>
+    <v-container>
+      <div v-for="job in jobs" :key="job.pk">
+        <p>{{ job.company_name }}:{{ job.job_title }}</p>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
+import { apiService } from '../common/api.service.js';
 export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
+  name: 'Home',
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  methods: {
+    getJobs() {
+      let endpoint = '/api/jobs/';
+      apiService(endpoint).then(data => {
+        this.jobs.push(...data.results);
+      });
+    },
+  },
+  created() {
+    this.getJobs();
+    console.log(this.jobs);
   },
 };
 </script>
