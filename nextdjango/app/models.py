820ce66528b9cdb32dc5396d19f0f78960b3c919
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-17 22:32:03 +0900

    modelsの修正

--- nextdjango/app/models.py
+++ nextdjango/app/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Post(models.Model):
   title = models.CharField("タイトル",max_length=50)
-  image = models.ImageField(upLoad_to="images" ,verbose_name="イメージ画像")
-  content = models.CharField("本文")
-  created_at = models.CharField("作成日",auto_now_add=True)
+  image = models.ImageField(upload_to="images",verbose_name="イメージ画像")
+  content = models.TextField("本文")
+  created_at = models.DateTimeField("作成日",auto_now_add=True)
 
   def __str__(self): #postのtitle(タイトル)を返す
     return self.title
