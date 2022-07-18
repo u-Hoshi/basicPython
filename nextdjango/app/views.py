8a37ca1d9862601b9c7bdaea9ceaf4ffc71fa2d9
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-18 16:18:32 +0900

    バックエンド実装

--- nextdjango/app/views.py
+++ nextdjango/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PostSerializers
+from .models import Post
 
 # Create your views here.
+
+class PostView(generics.ListAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializers
+
+
+class PostDetailView(generics.RetrieveAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializers
