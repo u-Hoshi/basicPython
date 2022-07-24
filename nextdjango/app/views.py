0eccc410ef4f777dd05aea4c3f5e34e6941086a7
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-24 23:04:58 +0900

    AllowAnyを記述

--- nextdjango/app/views.py
+++ nextdjango/app/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import PostSerializers
 from .models import Post
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 
 class PostView(generics.ListAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializers
+  permission_classes = (AllowAny,) # permission_classesにAllowAnyを記述することでログインしてなくてもアクセスできるようになる
 
 
 class PostDetailView(generics.RetrieveAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializers
+  permission_classes = (AllowAny,) # permission_classesにAllowAnyを記述することでログインしてなくてもアクセスできるようになる
