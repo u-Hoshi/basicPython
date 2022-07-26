eccd3e0d12f05e0569527df2ad5186a194465d37
u-Hoshi <yuta000707@yahoo.co.jp>
2022-07-26 18:24:16 +0900

    gcpとの繋ぎこみ

--- nextdjango/frontend/pages/_app.js
+++ nextdjango/frontend/pages/_app.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import '../styles/globals.css';
-import Layout from '../components/layout/layout';
+import Head from "next/head";
+import "../styles/globals.css";
+import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
